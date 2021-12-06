f4989121514cd0ea15db0a3fb324407209cce234
Karen Leon <kmleonp@correo.udistrital.edu.com>
2021-12-06 16:24:28 -0500

    Primer versión del taller 3

---
+++ poema.py
@@ -0,0 +1,8 @@
+print('Arde en tus ojos un misterio, virgen')
+print('esquiva y compañera.')
+print('No sé si es odio o es amor la lumbre')
+print('inagotable de tu aliaba negra.')
+print('Conmigo irás mientras proyecte sombra')
+print('mi cuerpo y quede a mi sandalia arena.')
+print('-¿Eres la sed o el agua en mi camino?-')
+print('Dime, virgen esquiva y compañera.')
